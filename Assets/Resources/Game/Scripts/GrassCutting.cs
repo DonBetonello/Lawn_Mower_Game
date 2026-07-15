b91b4e914c3806cc2db6c349233dd582ece62829
DonBetonello <alekseii2022@gmail.com>
2026-07-15 23:09:22 +0300

    Code refactoring and project files cleaning
    Code refactoring focused on reworking ingame currency based scripts.
    Also removed redundant files and changed project hierarchy

---
+++ Assets/Resources/Game/Scripts/GrassCutting.cs
@@ -0,0 +1,97 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrassCutting : MonoBehaviour
+{
+
+
+    [SerializeField] private bool isGrassGrowing = false;
+    [SerializeField] private float GrassRegrowTimer = 0;
+    
+
+    private MoneyHandler moneyHandler;
+
+    public Upgrades upgrades;
+
+    [SerializeField] public float MoneyUpgrade;
+    [SerializeField] public bool isGrassDownBool;
+    [SerializeField] private float GrassGoToPosition;
+    [SerializeField] private float GrassStartPosition = -0.5f;
+    [SerializeField] private GrassGradient Grass_Change_Color;
+    public Flower_Multiplyer_Sript Pick_Up_Flower_Money_Multiplyer;
+    private float Flower_Multiplyer = 1;
+
+
+    public static event System.Action OnGrassGotCut; // Event to notify when the grass is cut
+
+
+    void Start()
+    {
+        RandomizeGrassRotationAtStart();
+    }
+
+    
+    void FixedUpdate()
+    {
+        
+        if (isGrassDownBool == true)
+        {
+
+            GetGrassDown();
+            Grass_Change_Color.Grass_Changes_color();
+            isGrassDownBool = false;
+        }
+       
+
+        if (GrassRegrowTimer >= 5 - upgrades.GrowthUpgradeData.Value)
+        {
+            isGrassGrowing = false;
+            GetGrassUp();
+            GrassRegrowTimer = 0;
+        }
+        
+        
+        if (isGrassGrowing == true)
+        {
+            GrassRegrowTimer += 1 * Time.deltaTime;
+        }
+       
+    }
+    void OnTriggerEnter(Collider collision)
+    {
+       /* if (Pick_Up_Flower_Money_Multiplyer.flower_Money_Multiplyer == true)
+        {
+            Flower_Multiplyer = 2f;
+        }
+        else 
+        {
+            Flower_Multiplyer = 1f;
+        }*/
+        isGrassGrowing = true;
+        isGrassDownBool = true;
+        OnGrassGotCut?.Invoke(); // Notify subscribers that the grass has been cut
+    }
+    private void OnTriggerExit(Collider collision)
+    {
+        isGrassGrowing = true;
+        isGrassDownBool = true;
+    }
+    private void RandomizeGrassRotationAtStart()
+    {
+        transform.DORotate(new Vector3(-90, Random.Range(0, 360), 0), 0);
+    }
+    private void GetGrassDown()
+    {
+        transform.DOMove(new Vector3(transform.position.x, GrassGoToPosition, transform.position.z), 1);
+        transform.DORotate(new Vector3(-90, Random.Range(0, 360), 0), 0.5f);
+    }
+    private void GetGrassUp()
+    {
+        transform.DOMove(new Vector3(transform.position.x, GrassStartPosition, transform.position.z), 1);
+        transform.DORotate(new Vector3(-90, Random.Range(0, 360), 0), 1.5f);
+    }
+
+}
+
---
+++ Assets/Resources/Game/Scripts/MoneyHandlingScripts/MoneyHandler.cs
@@ -0,0 +1,64 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class MoneyHandler : MonoBehaviour 
+{
+    private float money;
+    
+    [SerializeField] private float amount;
+    private float earningUpgradeMultiplier = 1.0f; // Example multiplier for earning upgrades
+    private float flowerMultiplier = 1.0f; // Example multiplier for flower upgrades
+    private float flowerMultiplierTimer = 0f; // Timer for flower multiplier duration
+   [SerializeField] private Upgrades upgrades;
+
+
+    
+
+    public float GetMoney => money;
+
+    public void AddMoney(float amount)
+    {
+        money += calculateMoneyMultiplier(amount, flowerMultiplier, earningUpgradeMultiplier);
+    }
+    public void RemoveMoney(float amount) { money -= amount; }
+
+
+    private float calculateMoneyMultiplier(float baseAmount, float flowerMultiplier, float earningUpgradeMultiplier)
+    {
+        return baseAmount * flowerMultiplier * earningUpgradeMultiplier;
+    }
+
+
+    private void OnEnable(){
+        GrassCutting.OnGrassGotCut += OnGrassCut;
+    }
+
+    private void OnDisable()
+    {
+        GrassCutting.OnGrassGotCut -= OnGrassCut;
+    }
+
+    private void OnGrassCut()  // This method is called when the grass is cut, and it should add money to the player's total.  
+    {
+        AddMoney(amount);
+    }
+
+    private void OnFlowerMultiplyerPickedUp(float timer) // This method is called when the flower multiplier is picked up, and it should set the flower multiplier and start the timer. 
+    {
+        flowerMultiplierTimer += timer;
+        flowerMultiplier = 2f;
+    }
+
+
+    private void Update()
+    {
+        if (flowerMultiplierTimer > 0f) {
+            flowerMultiplierTimer -= Time.deltaTime;
+        }
+        else
+        {
+            flowerMultiplier = 1f;
+        }
+        earningUpgradeMultiplier = 1f + (upgrades.EarningUpgradeData.Value * 0.1f); // Each level increases earnings by 10%
+    }
+}
